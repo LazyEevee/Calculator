ca3d9528aaaa3b3fa2841a05c52d912baceaf526
Christine Dimmer <chrissy.1989@yahoo.co.uk>
2022-04-29 17:03:28 +0100

    buttons now drawn in for loop

--- main.py
+++ main.py
@@ -12,83 +12,34 @@ class Calculator:
         self.math = "clear"
         self.total = 0
 
-        self.button_1 = Button(main, text="1", width=4, height=1, command=lambda: self.button_click(1),
-                               font="Helvetica 18")
-        self.button_2 = Button(main, text="2", width=4, height=1, command=lambda: self.button_click(2),
-                               font="Helvetica 18")
-        self.button_3 = Button(main, text="3", width=4, height=1, command=lambda: self.button_click(3),
-                               font="Helvetica 18")
-        self.button_4 = Button(main, text="4", width=4, height=1, command=lambda: self.button_click(4),
-                               font="Helvetica 18")
-        self.button_5 = Button(main, text="5", width=4, height=1, command=lambda: self.button_click(5),
-                               font="Helvetica 18")
-        self.button_6 = Button(main, text="6", width=4, height=1, command=lambda: self.button_click(6),
-                               font="Helvetica 18")
-        self.button_7 = Button(main, text="7", width=4, height=1, command=lambda: self.button_click(7),
-                               font="Helvetica 18")
-        self.button_8 = Button(main, text="8", width=4, height=1, command=lambda: self.button_click(8),
-                               font="Helvetica 18")
-        self.button_9 = Button(main, text="9", width=4, height=1, command=lambda: self.button_click(9),
-                               font="Helvetica 18")
-        self.button_0 = Button(main, text="0", width=4, height=1, command=lambda: self.button_click(0),
-                               font="Helvetica 18")
-        self.button_equal = Button(main, text="=", width=4, height=1, command=lambda: self.button_calc("="),
-                                   font="Helvetica 18")
-        self.btn_clear = Button(main, text="AC", width=4, height=1, command=self.button_clear, font="Helvetica 18")
-        self.button_add = Button(main, text="+", width=4, height=1, command=lambda: self.button_calc("+"),
-                                 font="Helvetica 18")
-        self.button_subtract = Button(main, text="-", width=4, height=1, command=lambda: self.button_calc("-"),
-                                      font="Helvetica 18")
-        self.button_multiply = Button(main, text="*", width=4, height=1, command=lambda: self.button_calc("*"),
-                                      font="Helvetica 18")
-        self.button_divide = Button(main, text="/", width=4, height=1, command=lambda: self.button_calc("/"),
-                                    font="Helvetica 18")
-
         self.dark_mode = Checkbutton(main, text="Dark Mode", variable=self.var, command=self.colour)
         self.dark_mode.deselect()
 
         self.entry = Entry(main, width=18, borderwidth=5, justify="right", font="Helvetica 18")
         self.dark_mode.grid(row=0, column=1, columnspan=2)
         self.entry.grid(row=1, column=0, columnspan=5, padx=0, pady=5)
-        self.button_7.grid(row=2, column=0)
-        self.button_8.grid(row=2, column=1)
-        self.button_9.grid(row=2, column=2)
-        self.button_divide.grid(row=2, column=3)
-        self.button_4.grid(row=3, column=0)
-        self.button_5.grid(row=3, column=1)
-        self.button_6.grid(row=3, column=2)
-        self.button_multiply.grid(row=3, column=3)
-        self.button_1.grid(row=4, column=0)
-        self.button_2.grid(row=4, column=1)
-        self.button_3.grid(row=4, column=2)
-        self.button_subtract.grid(row=4, column=3)
-        self.btn_clear.grid(row=5, column=0)
-        self.button_0.grid(row=5, column=1)
-        self.button_equal.grid(row=5, column=2)
-        self.button_add.grid(row=5, column=3)
-
         self.entry.insert(0, "0")
 
         root.config(bg=self.back_colour)
         self.dark_mode.config(bg=self.back_colour, fg=self.fore_colour, selectcolor=self.back_colour,
                               activebackground=self.back_colour, activeforeground=self.fore_colour)
         self.entry.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_1.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_2.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_3.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_4.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_5.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_6.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_7.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_8.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_9.config(bg=self.back_colour, fg=self.fore_colour)
-        self.btn_clear.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_0.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_equal.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_add.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_subtract.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_multiply.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_divide.config(bg=self.back_colour, fg=self.fore_colour)
+
+        self.buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "AC", "0", "=", "+"]
+        i = 0
+        for c in self.buttons:
+            button = Button(main, text=c, width=4, height=1,
+                            bg=self.back_colour, fg=self.fore_colour,
+                            font="Helvetica 18")
+            button.grid(row=int(i / 4)+2, column=(i % 4))
+
+            if c in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0"):
+                button.config(command=lambda b=c: self.button_click(b))
+            elif c == "AC":
+                button.config(command=self.button_clear)
+            else:
+                button.config(command=lambda b=c: self.button_calc(b))
+            i += 1
 
     def colour(self):
         mode = str(self.var.get())
@@ -102,22 +53,21 @@ class Calculator:
         self.dark_mode.config(bg=self.back_colour, fg=self.fore_colour, selectcolor=self.back_colour,
                               activebackground=self.back_colour, activeforeground=self.fore_colour)
         self.entry.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_1.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_2.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_3.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_4.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_5.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_6.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_7.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_8.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_9.config(bg=self.back_colour, fg=self.fore_colour)
-        self.btn_clear.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_0.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_equal.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_add.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_subtract.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_multiply.config(bg=self.back_colour, fg=self.fore_colour)
-        self.button_divide.config(bg=self.back_colour, fg=self.fore_colour)
+
+        i = 0
+        for c in self.buttons:
+            button = Button(text=c, width=4, height=1,
+                            bg=self.back_colour, fg=self.fore_colour,
+                            font="Helvetica 18")
+            button.grid(row=int(i / 4) + 2, column=(i % 4))
+
+            if c in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0"):
+                button.config(command=lambda b=c: self.button_click(b))
+            elif c == "AC":
+                button.config(command=self.button_clear)
+            else:
+                button.config(command=lambda b=c: self.button_calc(b))
+            i += 1
 
     def button_click(self, number):
         if self.math == "equal":
